8c26c4e5215f973f1e089217801faea1d97b84e3
anilsagart <aniltsagar@gmail.com>
2020-04-19 20:12:58 +0530

    added lenzdrift.com path and built

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lenzdrift/precache-manifest.109fe1ccb2059bcbfbbd49c97f18edc8.js"
+  "/precache-manifest.4d545c81e5364ea38dbb9a9367882a69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lenzdrift/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
