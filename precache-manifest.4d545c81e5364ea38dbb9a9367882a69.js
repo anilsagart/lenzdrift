97480b2a5b8e2cf3fcd6d0c0c96c4e64762ee9ed
Anilsagar T (BLR GSS) <anilsagart@nousinfo.com>
2020-04-19 18:42:06 +0530

    pushing react 1st version code

---
+++ precache-manifest.4d545c81e5364ea38dbb9a9367882a69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6b2029cd7a858915524ec68a1d3973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "febde40d48bbdc0d2a47",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a188eafab24109ad1a00",
+    "url": "/static/css/main.adce5a89.chunk.css"
+  },
+  {
+    "revision": "febde40d48bbdc0d2a47",
+    "url": "/static/js/2.5f58b27d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.5f58b27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a188eafab24109ad1a00",
+    "url": "/static/js/main.16022422.chunk.js"
+  },
+  {
+    "revision": "809f1f44b213bbcae6ea",
+    "url": "/static/js/runtime-main.086b56a4.js"
+  }
+]);
