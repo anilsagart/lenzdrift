136a01b81dcd9b08ddc4e56c69441eaeeb5e7574
anilsagart <aniltsagar@gmail.com>
2021-01-08 16:45:42 +0530

    new version 2021

---
+++ assets/images/images.ts
@@ -0,0 +1,129 @@
+export const images = {
+
+    desktop: null,
+    mobile: {
+        "Celebrates": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpg",
+                location: "bangalore",
+                images: ["mobile/portfolio/1.jpg", "mobile/portfolio/2.jpg", "mobile/portfolio/2.jpg"],
+                byClient: "For optimal website performance, images should be as large as needed to fill their “containers”, based on your page layout. For example, slideshow images are usually wider; blog images are medium-sized (at a width equal to the page width minus the sidebar); thumbnails are smaller, etc."
+            },
+        ],
+        "Portfolio": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpg",
+                location: "bangalore",
+                images: ["mobile/portfolio/1.jpg", "mobile/portfolio/2.jpg", "mobile/portfolio/2.jpg"],
+                byClient: "Happy to work with Lenzdrift again"
+            },
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpg",
+                location: "bangalore",
+                images: ["mobile/portfolio/2.jpg"],
+                byClient: "Happy to work with Lenzdrift again"
+            },
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpg",
+                location: "bangalore",
+                images: ["mobile/portfolio/2.jpg"],
+                byClient: "Happy to work with Lenzdrift again"
+            },
+            {
+                title: "anil",
+                cover: "mobile/portfolio/1.jpg",
+                location: "mysore",
+                images: ["mobile/portfolio/1.jpg"],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+        "Birthday": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpeg",
+                location: "bangalore",
+                images: [
+                    "mobile/birthday/1.jpeg", "mobile/birthday/2.jpeg"
+                ],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+        "Kids": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpeg",
+                location: "bangalore",
+                images: ["mobile/kids/2.jpeg"],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+        "Candid Wedding": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpeg",
+                location: "bangalore",
+                images: [
+                    "mobile/birthday/1.jpeg", "mobile/birthday/2.jpeg"
+                ],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+        "Engagement": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpeg",
+                location: "bangalore",
+                images: ["mobile/engagement/1.jpeg"],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+        
+        "House Warming": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpeg",
+                location: "bangalore",
+                images: [
+                    "mobile/birthday/1.jpeg", "mobile/birthday/2.jpeg"
+                ],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+     
+        "MakeUp Shoot": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpeg",
+                location: "bangalore",
+                images: [
+                    "mobile/birthday/1.jpeg", "mobile/birthday/2.jpeg"
+                ],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+        "Maternity": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/2.jpeg",
+                location: "bangalore",
+                images: ["mobile/maternity/1.jpeg"],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ],
+       
+        "Pre Wedding": [
+            {
+                title: "dhanvi",
+                cover: "mobile/portfolio/1.jpeg",
+                location: "bangalore",
+                images: ["mobile/pre-wedding/1.jpeg"],
+                byClient: "Happy to work with Lenzdrift again"
+            }
+        ]
+    }
+
+}
